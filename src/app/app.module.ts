3efd52249b12cfc6789e19997c45bae7e0182ff0
samuu010 <samuel.alonso.de.gregorio@alumnojoyfe.iepgroup.es>
2023-10-18 12:47:09 +0200

    Footer Añadido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavbarComponent,
+        FooterComponent,
         
     ],
     providers: [],
